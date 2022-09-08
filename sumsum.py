1ac161bbec4362b6b12965ab464a33e5fcee1f26
skuljan <69480837+skuljan@users.noreply.github.com>
2022-09-08 20:12:36 +0300

    Add files via upload

---
+++ sumsum.py
@@ -0,0 +1,12 @@
+def exp_sum(n):
+    if n <= 0: return 0
+
+    waystosum = [1] + [0] * n
+
+    for i in range(1, n + 1):
+        for y in range(i, n + 1):
+            waystosum[y] += waystosum[y - i]
+    return waystosum[-1]
+
+
+print(exp_sum(50))
